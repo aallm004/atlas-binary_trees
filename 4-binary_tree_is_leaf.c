da1ee54879cfedcb5291d84e886e75cd2ea5146d
aallm004 <abigailrosea10@gmail.com>
2024-04-13 02:21:25 +0000

    added print_tree file

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ *binary-tree_is_leaf - function that checks if a node is a leaf
+ *
+ *@node: a pointer to the node to check
+ *
+ *Return: 1 if node is leaf, 0 otherwise
+ **/
+ int binary_tree_is_leaf(const binary_tree_t *node)
+ {
+	
+ }
