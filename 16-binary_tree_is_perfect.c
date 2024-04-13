46ea0b5339e904df025d61c3818e13dd69c3346e
aallm004 <abigailrosea10@gmail.com>
2024-04-13 18:52:24 +0000

    last betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  *@tree: pointer to the root node of the tree to check
  *
- *Return: 
+ *Return: 1 if success, 0 if not
  **/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
