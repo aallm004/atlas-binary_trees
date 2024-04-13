9ea11a9bc55830556f934d550c0035931238025d
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:45:08 +0000

    added binary_tree_node function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,6 +18,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (node == NULL)
 			return (NULL);
 
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+	
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
