a9249ec408b1b4aa7ec316675d2a996ba497be9d
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:42:22 +0000

    first push for 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,33 @@
 #include "binary_trees.h"
 /**
+ *binary_tree_insert_left - function that inserts a node as the left child
  *
+ *@parent: pointer to the node to insert the left-child in
+ *@value: value to store in the new node
+ *
+ *Return: pointer to the created node or NULL
  **/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (parent == NULL)
+	return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+		if (node == NULL)
+			return (NULL);
+
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}
+	parent->left = node;
+
+	return (node);
+
+}
+
+
+
