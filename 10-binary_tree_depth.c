361ee15934fb3c771fc36f13da0932d5c6ed7416
aallm004 <abigailrosea10@gmail.com>
2024-04-13 17:17:09 +0000

    second commit for depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,20 +8,12 @@
  **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_depth = 0;
-	size_t right_depth = 0;
+	size_t depth = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_depth = 1 + binary_tree_depth(tree->left);
-
-	if (tree->right)
-		right_depth = 1 + binary_tree_depth(tree->right);
-
-	if (left_depth > right_depth)
-		return (left_depth);
-	else
-		return (right_depth);
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
 }
