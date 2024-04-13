7b6e325ba34c943cb47fc42da911ba28492c6376
aallm004 <abigailrosea10@gmail.com>
2024-04-13 17:39:51 +0000

    +=

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,8 +16,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		leaves++;
 
-	leaves + binary_tree_leaves(tree->left);
-	leaves + binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
 
 	return (leaves);
 }
