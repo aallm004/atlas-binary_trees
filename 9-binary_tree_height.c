c4ffe2d8bccd6d6127b39584a86fd11c42a14417
aallm004 <abigailrosea10@gmail.com>
2024-04-13 16:55:38 +0000

    mistype line 23

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->right)
 		right_height = 1 + binary_tree_height(tree->right);
 
-	if (right_height < right_height)
+	if (right_height < left_height)
 		return (right_height);
 	else
 		return (left_height);
