adc65bef71bef03c567bd152ce05741da75a0baa
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:47:53 +0000

    malloc is included in new function?

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,10 +14,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 	return (NULL);
 
-	node = malloc(sizeof(binary_tree_t));
-		if (node == NULL)
-			return (NULL);
-
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
