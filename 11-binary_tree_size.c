4b3f41453d509f75f8d23ec004964087c4cffcb4
aallm004 <abigailrosea10@gmail.com>
2024-04-13 17:27:17 +0000

    +=

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,7 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 	
 	size = binary_tree_size(tree->left);
-	size = binary_tree_size(tree->right);
+	size += binary_tree_size(tree->right);
 
 	return (size);
 }
