5cc44e274c600515aa8dc2e675c71956ec88dcc4
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:52:22 +0000

    first push number 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right - function that inserts a node as the right child
+ *
+ *@parent: pointer to the node to insert the left-child in
+ *@value: value to store in the new node
+ *
+ *Return: pointer to the created node or NULL
+ **/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (parent == NULL)
+	return (NULL);
+
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+
+	return (node);
+
+}
