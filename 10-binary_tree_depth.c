53771274f3011a6b4831cd033fa9d0b922638035
aallm004 <abigailrosea10@gmail.com>
2024-04-13 17:07:26 +0000

    first commit for task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,27 @@
+include "binary_trees.h"
+/**
+ *binary_tree_depth - func measures depth of a node
+ *
+ *@tree: pointer to the node to measure depth
+ *
+ *Return: depth, or 0
+ **/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t left_depth = 0;
+	size_t right_depth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_depth = 1 + binary_tree_depth(tree->left);
+
+	if (tree->right)
+		right_depth = 1 + binary_tree_depth(tree->right);
+
+	if (left_depth > right_depth)
+		return (left_depth);
+	else
+		return (right_depth);
+}
