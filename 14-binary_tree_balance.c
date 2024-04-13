5899301d767682e0ccf0d89fdab52f860e060fd0
aallm004 <abigailrosea10@gmail.com>
2024-04-13 18:06:38 +0000

    betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
-		return (right_height +1);
+		return (right_height + 1);
 }
 
 /**
@@ -39,9 +39,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
- 	int left_height, right_height;
+	int left_height, right_height;
 
- 	if (tree == NULL)
+	if (tree == NULL)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
