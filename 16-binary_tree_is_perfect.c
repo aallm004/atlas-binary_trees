5e9677917637243fe0efccec37d1cc38e395a80b
aallm004 <abigailrosea10@gmail.com>
2024-04-13 18:47:19 +0000

    betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,18 +43,18 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	
-	if (tree->left == NULL || tree-right == NULL)
+
+	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
 	left = binary_tree_is_perfect(tree->left);
 	right = binary_tree_is_perfect(tree->right);
-	
+
 	if (left == 0 || right == 0)
 		return (0);
-	
+
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 		return (1);
-	
-	return(0);
+
+	return (0);
 }
