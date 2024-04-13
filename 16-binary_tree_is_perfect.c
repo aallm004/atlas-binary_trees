8f12bfead24f27aa472e63333de7a6de8fe7952d
aallm004 <abigailrosea10@gmail.com>
2024-04-13 18:35:30 +0000

    added is height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,29 @@
 #include "binary_trees.h"
+
+/**
+ *binary_tree_height - measures the height of binary tree
+ *
+ *@tree: pointer to root node of tree to measure height
+ *
+ *Return: height of tree 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
 /**
  *binary_tree_is_perfect - func that checks if a binary tree is perfect
  *
