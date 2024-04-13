e64cd90f626809bd332de0473c893e16791d7fc0
aallm004 <abigailrosea10@gmail.com>
2024-04-13 16:56:36 +0000

    switch left and right

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,8 +20,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->right)
 		right_height = 1 + binary_tree_height(tree->right);
 
-	if (right_height < left_height)
-		return (right_height);
-	else
+	if (left_height > right_height)
 		return (left_height);
+	else
+		return (right_height);
 }
