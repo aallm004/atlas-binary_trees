f56685090689fcc7250b36b5673e36ec9a744cc8
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:03:32 +0000

    added ; after creating node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,11 +10,11 @@
  **/
  binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
- binary_tree_t *node
+ binary_tree_t *node;
 
  node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
-	return (NULL);
+		return (NULL);
 
 node->n = value;
 node->left = NULL;
