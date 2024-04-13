2f4d1fea267be9780bfd99f5873a29a9889f1f50
aallm004 <abigailrosea10@gmail.com>
2024-04-13 16:00:10 +0000

    new approach to 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,9 +8,8 @@
  **/
  int binary_tree_is_leaf(const binary_tree_t *node)
  {
-		if (!node)
-			return (0);
+	if (node && !node->left && node->right)
+		return (1);
 
-		else
-		return(!node->left && !node->right);
- }
+	return(0);
+}
