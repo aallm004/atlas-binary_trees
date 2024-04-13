4ca65c03278f78f4109454a173342d1e16b14642
aallm004 <abigailrosea10@gmail.com>
2024-04-13 16:03:10 +0000

    added if node == NULL

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,9 @@
  **/
  int binary_tree_is_leaf(const binary_tree_t *node)
  {
-	if (node && !node->left && node->right)
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
 		return (1);
-
 	return(0);
 }
