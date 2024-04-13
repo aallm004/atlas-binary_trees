d47cd9768485833eebd56513898a0f13a5e0ed2f
aallm004 <abigailrosea10@gmail.com>
2024-04-13 01:46:06 +0000

    again

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			return (NULL);
 
 	node = binary_tree_node(parent, value);
-	if (new == NULL)
+	if (node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
